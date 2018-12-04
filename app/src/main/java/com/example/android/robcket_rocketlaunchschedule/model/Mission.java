add23152ddb994a8ee251d5bfc861551784cdb92
Roby Hartono <roby.hartono@ges.thm.de>
2018-12-04 23:03:11 +0100

    Added Model Mission

---
+++ app/src/main/java/com/example/android/robcket_rocketlaunchschedule/model/Mission.java
@@ -0,0 +1,99 @@
+package com.example.android.robcket_rocketlaunchschedule.model;
+
+import java.util.List;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Mission {
+
+    @SerializedName("id")
+    @Expose
+    private int id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("type")
+    @Expose
+    private int type;
+    @SerializedName("wikiURL")
+    @Expose
+    private String wikiURL;
+    @SerializedName("typeName")
+    @Expose
+    private String typeName;
+    @SerializedName("agencies")
+    @Expose
+    private List<Agency> agencies = null;
+    @SerializedName("payloads")
+    @Expose
+    private List<Payload> payloads = null;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getWikiURL() {
+        return wikiURL;
+    }
+
+    public void setWikiURL(String wikiURL) {
+        this.wikiURL = wikiURL;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public List<Agency> getAgencies() {
+        return agencies;
+    }
+
+    public void setAgencies(List<Agency> agencies) {
+        this.agencies = agencies;
+    }
+
+    public List<Payload> getPayloads() {
+        return payloads;
+    }
+
+    public void setPayloads(List<Payload> payloads) {
+        this.payloads = payloads;
+    }
+
+}
