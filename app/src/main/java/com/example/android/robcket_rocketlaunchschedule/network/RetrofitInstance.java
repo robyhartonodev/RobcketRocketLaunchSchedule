1ed2946054c0d85fa028a0b3e8ee28a61e83ed1f
Roby Hartono <roby.hartono@ges.thm.de>
2018-12-05 00:28:55 +0100

    Updated Network RetrofitInstance

--- app/src/main/java/com/example/android/robcket_rocketlaunchschedule/network/RetrofitInstance.java
+++ app/src/main/java/com/example/android/robcket_rocketlaunchschedule/network/RetrofitInstance.java
@@ -12,7 +12,7 @@ public class RetrofitInstance {
     /**
      * Create an instance of Retrofit object
      * */
-    public static Retrofit getRetrofitInstanceRocket() {
+    public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
             retrofit = new retrofit2.Retrofit.Builder()
                     .baseUrl(BASE_URL)
