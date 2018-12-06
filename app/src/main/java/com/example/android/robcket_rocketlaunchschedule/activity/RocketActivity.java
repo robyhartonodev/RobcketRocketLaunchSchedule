bc6a6f4768961729b8d3e96d56820673224442e0
Roby Hartono <roby.hartono@ges.thm.de>
2018-12-07 00:25:26 +0100

    Added RocketActivity and Layout

---
+++ app/src/main/java/com/example/android/robcket_rocketlaunchschedule/activity/RocketActivity.java
@@ -0,0 +1,32 @@
+package com.example.android.robcket_rocketlaunchschedule.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.android.robcket_rocketlaunchschedule.R;
+
+public class RocketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rocket);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
