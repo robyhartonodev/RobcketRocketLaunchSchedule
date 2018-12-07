7500ddf8a2966643a1c90c56495e27aa0faaaa21
Roby Hartono <roby.hartono@ges.thm.de>
2018-12-07 03:12:39 +0100

    Added Navigation Drawer in the MainActivity

--- app/src/main/java/com/example/android/robcket_rocketlaunchschedule/activity/MainActivity.java
+++ app/src/main/java/com/example/android/robcket_rocketlaunchschedule/activity/MainActivity.java
@@ -19,6 +19,12 @@ import com.example.android.robcket_rocketlaunchschedule.model.Rocket;
 import com.example.android.robcket_rocketlaunchschedule.model.RocketList;
 import com.example.android.robcket_rocketlaunchschedule.my_interface.GetRocketDataService;
 import com.example.android.robcket_rocketlaunchschedule.network.RetrofitInstance;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.stephentuso.welcome.WelcomeHelper;
 
 import java.util.ArrayList;
@@ -70,6 +76,27 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        //if you want to update the items at a later time it is recommended to keep it in a variable
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName(R.string.drawer_item_home);
+        SecondaryDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(2).withName(R.string.drawer_item_settings);
+
+        //create the drawer and remember the `Drawer` result object
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .addDrawerItems(
+                        item1,
+                        new DividerDrawerItem(),
+                        item2
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        // do something with the clicked item :D
+                        return true;
+                    }
+                })
+                .build();
     }
 
 
