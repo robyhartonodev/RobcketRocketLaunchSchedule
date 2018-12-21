c2d71a6052fe4aa5baa4235da7f863803252940d
Roby Hartono <roby.hartono@ges.thm.de>
2018-12-21 16:57:15 +0100

    Updated RetrofitInstance with rx2java2adapter

--- app/src/main/java/com/example/android/robcket_rocketlaunchschedule/network/RetrofitInstance.java
+++ app/src/main/java/com/example/android/robcket_rocketlaunchschedule/network/RetrofitInstance.java
@@ -1,6 +1,8 @@
 package com.example.android.robcket_rocketlaunchschedule.network;
 
 
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -17,6 +19,17 @@ public class RetrofitInstance {
             retrofit = new retrofit2.Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+    public static Retrofit getRetrofitRxInstance(){
+        if (retrofit == null) {
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .build();
         }
         return retrofit;
