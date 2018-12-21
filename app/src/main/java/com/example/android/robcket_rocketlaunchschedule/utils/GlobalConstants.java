ab2052a5c67dcb0d698cff2c05682f2909bf7867
Roby Hartono <roby.hartono@ges.thm.de>
2018-12-21 16:57:43 +0100

    Added GlobalConstants file to store preferences variables

---
+++ app/src/main/java/com/example/android/robcket_rocketlaunchschedule/utils/GlobalConstants.java
@@ -0,0 +1,13 @@
+package com.example.android.robcket_rocketlaunchschedule.utils;
+
+/**
+ * This class contains global variables for Keys, Tag, Preferences, Action ID, etc.
+ */
+public final class GlobalConstants {
+
+
+    private GlobalConstants(){
+
+    }
+
+}
